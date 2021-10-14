35623d521f0b175951a322c562e8e596b7122bf0
pinkfish <pinkfishfrog@gmail.com>
2021-10-14 22:26:19 +0000

    A few more updates to make things more null safe.

---
+++ example/lib/camera_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+///
+/// A nice view of the camera details
+///
+class CameraView extends StatelessWidget {
+  final Uri url;
+
+  const CameraView({required this.url, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,47 +3,27 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:canon_ccapi/canon_ccapi.dart';
+import 'package:upnp/upnp.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CanonCCAPIDemo());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class CanonCCAPIDemo extends StatefulWidget {
+  const CanonCCAPIDemo({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<CanonCCAPIDemo> createState() => _CanonCCAPIDemoState();
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+class _CanonCCAPIDemoState extends State<CanonCCAPIDemo> {
+  var discoverer = DeviceDiscoverer();
+
 
   @override
   void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await CanonCcapi.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+super.initState();
+discoverer.getDevices(type: )
   }
 
   @override
@@ -54,7 +34,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: ,
         ),
       ),
     );
